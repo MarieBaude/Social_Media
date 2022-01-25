37038d8f69bea45f1f2934d57fee6241dc3397ca
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-25 15:53:07 +0100

    delete comment in html

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+
 import App from "./App";
-import index from "./index.css";
+import "./index.css";
 
 
 ReactDOM.render(
