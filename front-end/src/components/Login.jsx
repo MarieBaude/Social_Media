6faefdcd3921d15c1c81eec6f04330e4f11e760b
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-25 15:15:59 +0100

    adding button and that css

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -20,6 +20,25 @@ const Login = () => {
           autoPlay
           className='w-full h-full object-cover'
         />
+        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+          <div className='p-5'>
+            <img src={logo} width='130px' alt='logo' />
+          </div>
+          <div className='shadow-2x1'>
+            <GoogleLogin 
+              clientId=''
+              render={(renderProps) => (
+                <button 
+                type='button'
+                className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'>
+                <FcGoogle 
+                  className='mr-4'
+                />
+                </button>
+              )}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
