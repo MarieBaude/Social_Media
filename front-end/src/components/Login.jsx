2c21fda853e7b011c523a30cd6dfeb10fa2e43aa
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-27 14:01:04 +0100

    fix Login.jsx

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -10,19 +10,7 @@ import { client } from '../client';
 const Login = () => {
   const navigate = useNavigate(); 
   const responseGoogle = (response) => {
-    localStorage.setItem('user', JSON.stringify(response.profileObj));
-    const {name, googleId, imageUrl} = response.profileObj;
-    const doc = {
-      _id:googleId,
-      _type:'user',
-      userName:name,
-      image:imageUrl
-    }
-
-    client.createIfNotExists(doc)
-      .then(() => {
-        navigate('/', { replace: true }) 
-      })
+    
   }
   return (
     <div className='flex justify-start items-center flex-col h-screen'>
