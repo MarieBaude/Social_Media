5d333b0ede15f6179ecf8b94f976cf211d4fb93d
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-27 10:38:34 +0100

    adding onclick in link

--- front-end/src/components/Sidebar.jsx
+++ front-end/src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = (user, closeToggle) => {
   return (
     <div className='flex flex-col justify-between bg-white h-full overflow-y-scrikk min-w-210 hide-scrollbar'>
       <div className='flex flex-col'>
-      <Link to='/' className='flex px-5 gap-2 my-6 pt-1 w-190 items-center'>
+      <Link to='/' className='flex px-5 gap-2 my-6 pt-1 w-190 items-center' onClick={handleCloseSidebar}>
         <img src={logo} alt='logo' className='w-full' />
       </Link>
       </div>
