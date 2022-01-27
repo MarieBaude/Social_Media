7387cc86e296fcb92d2ff79a72de57a9d6ced6b6
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-27 16:42:36 +0100

    add feedQuery in Feed component

--- front-end/src/components/Feed.jsx
+++ front-end/src/components/Feed.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 import {client} from '../client';
-import { searchQuery } from '../utils/data';
+import { feedQuery, searchQuery } from '../utils/data';
 import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 
@@ -18,7 +18,10 @@ const Feed = () => {
         setLoading(false);
       })
     } else { 
-
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      })
     }
   }, [categoryId]);
   
