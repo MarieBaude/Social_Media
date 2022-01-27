cedd8929ed3b59202c7388151767a4b14101419d
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-27 10:58:42 +0100

    adding categories

--- front-end/src/components/Sidebar.jsx
+++ front-end/src/components/Sidebar.jsx
@@ -8,6 +8,14 @@ import Home from '../container/Home';
 const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
 const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
 
+const categories = [
+  {name : 'Animals'},
+  {name : 'Wallapapers'},
+  {name : 'Photographie'},
+  {name : 'Gamers'},
+  {name : 'Coding'}
+]
+
 const Sidebar = (user, closeToggle) => {
   const handleCloseSidebar = () => {
     if (closeToggle) closeToggle(false);
