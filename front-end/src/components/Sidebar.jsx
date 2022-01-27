87bcca5dc527f757faa589ba898e2d351ba25bf7
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-27 10:37:28 +0100

    adding link with css in sidebar

--- front-end/src/components/Sidebar.jsx
+++ front-end/src/components/Sidebar.jsx
@@ -8,8 +8,8 @@ const Sidebar = (user, closeToggle) => {
   return (
     <div className='flex flex-col justify-between bg-white h-full overflow-y-scrikk min-w-210 hide-scrollbar'>
       <div className='flex flex-col'>
-      <Link>
-        
+      <Link to='/' className='flex px-5 gap-2 my-6 pt-1 w-190 items-center'>
+        <img src={logo} alt='logo' className='w-full' />
       </Link>
       </div>
     </div>
