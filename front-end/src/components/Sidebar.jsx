d7581ef60e60d637a150f36f15d17daee79fbbc8
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-27 10:52:06 +0100

    adding const isNotActiveStyle and isActive style with different css

--- front-end/src/components/Sidebar.jsx
+++ front-end/src/components/Sidebar.jsx
@@ -4,6 +4,9 @@ import { RiHomeFill } from 'react-icons/ri';
 import { IoIosArrowForward } from 'react-icons/io';
 import logo from '../assets/logo.png'
 
+const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
+const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
+
 const Sidebar = (user, closeToggle) => {
   const handleCloseSidebar = () => {
     if (closeToggle) closeToggle(false);
