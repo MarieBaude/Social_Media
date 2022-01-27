bc22a49f0ea28ce8b6879e26d1b95605880a7d14
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-27 10:47:06 +0100

    adding NavLink with css and function isActive or not

--- front-end/src/components/Sidebar.jsx
+++ front-end/src/components/Sidebar.jsx
@@ -12,9 +12,14 @@ const Sidebar = (user, closeToggle) => {
   return (
     <div className='flex flex-col justify-between bg-white h-full overflow-y-scrikk min-w-210 hide-scrollbar'>
       <div className='flex flex-col'>
-      <Link to='/' className='flex px-5 gap-2 my-6 pt-1 w-190 items-center' onClick={handleCloseSidebar}>
-        <img src={logo} alt='logo' className='w-full' />
-      </Link>
+        <Link to='/' className='flex px-5 gap-2 my-6 pt-1 w-190 items-center' onClick={handleCloseSidebar}>
+          <img src={logo} alt='logo' className='w-full' />
+        </Link>
+        <div className='flex flex-col gap-5'>
+          <NavLink to='/' className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle } >
+
+          </NavLink>
+        </div>
       </div>
     </div>
   );
