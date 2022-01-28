bd2ad5fc3400946dbd24c888901db92b76ee1d43
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-28 11:03:17 +0100

    adding css for appear button in image pin

--- front-end/src/components/Pin.jsx
+++ front-end/src/components/Pin.jsx
@@ -7,12 +7,18 @@ import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
 import { client, urlFor } from '../client';
 
 const Pin = ({ pin: { postedBy, image, _id, destination } }) => {
+  const [postHovered, setPostHovered] = useState(false);
+  const [savingPost, setSavingPost] = useState(false);
+
+  const navigate = useNavigate();
+
   return (
     <div className='m-2'>
       <div 
       onMouseEnter={() => setPostHovered(true)} 
       onMouseLeave={() => setPostHovered(false)} 
       onClick={() => navigate(`/pin-detail/${_id}`)}
+      className='relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out'
       >
 
       </div>
