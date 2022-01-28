a83a4e7f5054017b238844d308f91df29edfde69
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-28 22:39:17 +0100

    adding new rule for lenght for destination

--- front-end/src/components/Pin.jsx
+++ front-end/src/components/Pin.jsx
@@ -89,8 +89,8 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                     className='bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:100 hover:shadow-md'
                   >
                     <BsFillArrowUpRightCircleFill />
-                    { destination.length > 20 ? destination.slice(8, 20) : destination.slice(8) }
-                  </a>
+                    {destination.length > 15 ? `${destination.slice(0, 15)}...` : destination}
+                  </a> // voir pour retirer le http://wwww devant les images
                 )}
 
                 { postedBy?._id === user.googleId && (
