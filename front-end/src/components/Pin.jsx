f51409c895aab75a3923c8a725383098e5ab5181
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-28 12:46:09 +0100

    adding name user under pin

--- front-end/src/components/Pin.jsx
+++ front-end/src/components/Pin.jsx
@@ -115,7 +115,9 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
           src={postedBy?.image}
           alt='user-profile'
         />
-
+        <p className='font-semibold capitalize'>
+          {postedBy?.userName}            
+        </p>
 
       </Link>
     </div>
