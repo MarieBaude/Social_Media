0995e01a4048170f7975862833bb7f68b7f6de0d
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-28 10:57:23 +0100

    adding div with Mouse enter et Mouse Leave

--- front-end/src/components/Pin.jsx
+++ front-end/src/components/Pin.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import { MdDownloadForOfline } from 'react-icons/md';
 import { AiTwotoneDelete } from 'react-icons/ai';
@@ -8,7 +8,14 @@ import { client, urlFor } from '../client';
 
 const Pin = ({ pin: { postedBy, image, _id, destination } }) => {
   return (
-    <div>
+    <div className='m-2'>
+      <div 
+      onMouseEnter={() => setPostHovered(true)} 
+      onMouseLeave={() => setPostHovered(false)} 
+      onClick={() => navigate(`/pin-detail/${_id}`)}
+      >
+
+      </div>
       <img className='rounded-lg w-full' alt='user-post' src={urlFor(image).width(250).url()} />
     </div>
   );
