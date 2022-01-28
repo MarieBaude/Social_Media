44d244ed39a7e87d9cbc84fa2c2e4b8bb42df9f6
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-28 12:44:50 +0100

    adding user image profile  under pin

--- front-end/src/components/Pin.jsx
+++ front-end/src/components/Pin.jsx
@@ -101,12 +101,23 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                   >
                     <AiTwotoneDelete />
                   </button>
-                )}
-              
+                )}             
             </div>
           </div>
         )}
       </div>
+      <Link 
+        to={`user-profile/${user?._id}`}
+        className='flex gap-2 mt-2 items-center'
+      >
+        <img 
+          className='w-8 h-8 rounded-full object-cover'
+          src={postedBy?.image}
+          alt='user-profile'
+        />
+
+
+      </Link>
     </div>
   );
 };
