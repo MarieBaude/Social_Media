eb99007aef2ad0f69dd9e8d6d33b76c452615054
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-28 11:11:28 +0100

    adding icon for download pin

--- front-end/src/components/Pin.jsx
+++ front-end/src/components/Pin.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
-import { MdDownloadForOfline } from 'react-icons/md';
+import { MdDownloadForOffline } from 'react-icons/md';
 import { AiTwotoneDelete } from 'react-icons/ai';
 import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
 import { client, urlFor } from '../client';
@@ -20,9 +20,26 @@ const Pin = ({ pin: { postedBy, image, _id, destination } }) => {
       onClick={() => navigate(`/pin-detail/${_id}`)}
       className='relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out'
       >
-
+        <img className='rounded-lg w-full' alt='user-post' src={urlFor(image).width(250).url()} />
+        {postHovered && (
+          <div 
+            className='absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50'
+            style={{ height: '100%' }}
+          >
+            <div className='flex items-center justify-between'>
+              <div className='flex gap-2'>
+                <a 
+                  href={ `${image?.asset?.url}?dl=` } 
+                  download
+                  onClick={ (e) => e.stopPropagation() }
+                >
+                  <MdDownloadForOffline />
+                </a>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-      <img className='rounded-lg w-full' alt='user-post' src={urlFor(image).width(250).url()} />
     </div>
   );
 };
