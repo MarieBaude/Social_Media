0433f3905947c89a3f462013c25b430b1f8d6387
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-28 22:14:51 +0100

    adding button posted style

--- front-end/src/components/PinDetail.jsx
+++ front-end/src/components/PinDetail.jsx
@@ -91,7 +91,11 @@ const PinDetail = ({ user }) => {
             </div>
             <div className="flex flex-wrap mt-6 gap-3">
               <Link to={`/user-profile/${pinDetail?.postedBy._id}`} className="flex gap-2 mt-5 items-center bg-white rounded-lg ">
-                <img src={pinDetail?.postedBy.image} className="w-10 h-10 rounded-full" alt="user-profile" />
+                <img 
+                  className="w-10 h-10 rounded-full cursor-pointer" 
+                  src={pinDetail?.postedBy.image} 
+                  alt="user-profile" 
+                />
               </Link>
               <input
                 className='flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300' 
@@ -99,9 +103,14 @@ const PinDetail = ({ user }) => {
                 placeholder="Add a comment"
                 value={comment}
                 onChange={(e) => setComment(e.target.value)}
+              />
+              <button 
+                type="button"
+                className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none"
+                //onClick={addComment}
               >
-
-              </input>
+                {addingComment ? 'Posting the comment...' : 'Posted !'}
+              </button>
             </div>
           </div>
     </div>
