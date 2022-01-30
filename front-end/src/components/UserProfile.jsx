bb244bbb723a7d28ca3dfe872d137af7f868680e
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-30 14:41:11 +0100

    adding use state and use effect in import

--- front-end/src/components/UserProfile.jsx
+++ front-end/src/components/UserProfile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { useParams, useNavigate } from 'react-router-dom';
 import { GoogleLogout } from 'react-google-login';
@@ -9,8 +9,8 @@ import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 
 const UserProfile = () => {
-  const [user, setUser] = useState();
-  const [pins, setPins] = useState();
+  const [user, setUser] = useState(null);
+  const [pins, setPins] = useState(null);
   const [text, setText] = useState('Created');
   const [activeBtn, setActiveBtn] = useState('created');
 
