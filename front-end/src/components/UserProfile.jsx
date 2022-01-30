91df67fefe2b5ceef6a6de21c941798ecfbb4401
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-30 16:21:06 +0100

    adding condition, if no pin posted, message showing

--- front-end/src/components/UserProfile.jsx
+++ front-end/src/components/UserProfile.jsx
@@ -125,6 +125,13 @@ const UserProfile = () => {
             <MasonryLayout pins={pins} />
           </div>
 
+          {pins?.length === 0 && (
+            <div className="flex justify-center font-bold items-center w-full text-1xl mt-2">
+              No Pins Found!
+            </div>
+          )}
+          
+
         </div>
       </div>
     </div>
