eeeff81c5a260232fcecbf75ec05c0c16948dc50
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-30 16:26:18 +0100

    adding list of the categories from date

--- front-end/src/components/Sidebar.jsx
+++ front-end/src/components/Sidebar.jsx
@@ -4,20 +4,13 @@ import { RiHomeFill } from "react-icons/ri";
 import { IoIosArrowForward } from "react-icons/io";
 import logo from "../assets/logo.png";
 import Home from "../container/Home";
+import { categories } from "../utils/data";
 
 const isNotActiveStyle =
   "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize";
 const isActiveStyle =
   "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
 
-const categories = [
-  { name: "animals" },
-  { name: "wallapapers" },
-  { name: "photography" },
-  { name: "gaming" },
-  { name: "coding" },
-  { name: "other" },
-];
 
 const Sidebar = ({ user, closeToggle }) => {
   const handleCloseSidebar = () => {
