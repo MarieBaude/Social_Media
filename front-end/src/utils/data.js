3cf5abaddee1b78b70f38421a9f6a641e92139a1
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-01-31 15:13:43 +0100

    changing data

--- front-end/src/utils/data.js
+++ front-end/src/utils/data.js
@@ -193,7 +193,6 @@ export const userCreatedPinsQuery = (userId) => {
   return query;
 };
 
-
 export const userSavedPinsQuery = (userId) => {
   const query = `*[_type == 'pin' && '${userId}' in save[].userId ] | order(_createdAt desc) {
     image{
